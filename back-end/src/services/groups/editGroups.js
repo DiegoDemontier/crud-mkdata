c19766195aa66167c32dc471fce60a75dc2dc36a
Demontier <demontier@outlook.com>
2022-05-06 14:10:59 -0300

    feat: adiciona o services deleteGroup

--- back-end/src/services/groups/editGroups.js
+++ back-end/src/services/groups/editGroups.js
@@ -1,14 +1,14 @@
 const { grupos } = require('../../database/models');
 const { groupSchema } = require('../../utils/schemas');
 const errorConstructor = require('../../utils/errorHandling');
-const { Conflict, badRequest } = require('../../utils/statusCode');
+const { notFound, badRequest } = require('../../utils/statusCode');
 
 module.exports = async (nome, ativo) => {
   const { error } = groupSchema.validate({ nome, ativo });
   if (error) throw errorConstructor(badRequest, error.message);
 
   const nomeExists = await grupos.findOne({ where: { nome } });
-  if (!nomeExists) throw errorConstructor(Conflict, 'Grupo não existe');
+  if (!nomeExists) throw errorConstructor(notFound, 'Grupo não existe');
 
   await grupos.update({ ativo }, { where: { nome } });
 
