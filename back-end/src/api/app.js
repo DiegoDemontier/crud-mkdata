289c8ce3812dd3f57d06cddba245e28b5c9e48c2
Demontier <demontier@outlook.com>
2022-05-06 12:04:40 -0300

    feat: adiciona chamada do middleware de erro

--- back-end/src/api/app.js
+++ back-end/src/api/app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 
+const errorMiddleware = require('../middleware/errorHandle');
 const groupsRoutes = require('../router/groups');
 
 const app = express();
@@ -9,5 +10,6 @@ app.use(express.json());
 app.use(cors());
 
 app.use('/groups', groupsRoutes);
+app.use(errorMiddleware);
 
 module.exports = app;
