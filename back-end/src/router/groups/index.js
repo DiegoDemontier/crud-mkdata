e4ebef37f1c65e8b5253a21682c46eea9cb30a5a
Demontier <demontier@outlook.com>
2022-05-06 14:09:53 -0300

    feat: adiciona o endpoint para deletar um grupo

--- back-end/src/router/groups/index.js
+++ back-end/src/router/groups/index.js
@@ -1,9 +1,14 @@
 const express = require('express');
-const { createGroups, editGroups } = require('../../controllers/groups');
+const {
+  createGroups,
+  editGroups,
+  deleteGroups,
+} = require('../../controllers/groups');
 
 const routerUsers = express.Router();
 
 routerUsers.post('/', createGroups);
 routerUsers.put('/', editGroups);
+routerUsers.delete('/:id', deleteGroups);
 
 module.exports = routerUsers;
