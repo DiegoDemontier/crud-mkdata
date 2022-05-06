3728a9048bd053cb43d2ffa4c094472f887165fa
Demontier <demontier@outlook.com>
2022-05-06 15:14:11 -0300

    feat: adiciona o controller findAllGroups

--- back-end/src/controllers/groups/index.js
+++ back-end/src/controllers/groups/index.js
@@ -1,9 +1,11 @@
 const createGroups = require('./createGroups');
 const editGroups = require('./editGroups');
 const deleteGroups = require('./deleteGroups');
+const findAllGroups = require('./findAllGroups');
 
 module.exports = {
   createGroups,
   editGroups,
   deleteGroups,
+  findAllGroups,
 };
