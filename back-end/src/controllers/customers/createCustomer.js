a85bca8330058ad59f30b5c5b545524ea38eb7b4
Demontier <demontier@outlook.com>
2022-05-06 20:04:02 -0300

    refactor: controller createCustomer

--- back-end/src/controllers/customers/createCustomer.js
+++ back-end/src/controllers/customers/createCustomer.js
@@ -3,10 +3,9 @@ const createCustomer = require('../../services/customers/createCustomer');
 
 module.exports = async (req, res, next) => {
   try {
-    // const { nomeCliente, ativo, tipo, dadoDocumento, nomeDocumento } = req.body;
     await createCustomer(req.body);
     
-    return res.status(created).json({ message: 'Grupo criado com sucesso' });
+    return res.status(created).json({ message: 'Cliente criado com sucesso' });
   } catch (error) {
     console.log(`ERROR CREATE CUSTOMER -> ${error.message}`);
     return next(error);
