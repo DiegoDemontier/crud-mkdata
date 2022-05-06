a598ec41040a7926b40a63d9386676d7f9607c89
Demontier <demontier@outlook.com>
2022-05-06 20:19:42 -0300

    feat: adiciona schema para validar customer

--- back-end/src/services/customers/createCustomer.js
+++ back-end/src/services/customers/createCustomer.js
@@ -1,9 +1,14 @@
 const { clientes, documentos, documentosClientes, grupos } = require('../../database/models');
-// const { groupSchema } = require('../../utils/schemas');
+const { customerSchema } = require('../../utils/schemas');
+const { badRequest } = require('../../utils/statusCode');
+const errorConstructor = require('../../utils/errorHandling');
 
 // eslint-disable-next-line max-lines-per-function
 module.exports = async (data) => {
   const { nomeCliente, tipo, ativo, grupo, documentosCliente } = data;
+
+  const { error } = customerSchema.validate({ nomeCliente, tipo, ativo, grupo, documentosCliente });
+  if (error) throw errorConstructor(badRequest, error.message);
 
   const getGroup = await grupos.findOne({ where: { nome: grupo } });
 
--- back-end/src/utils/schemas.js
+++ back-end/src/utils/schemas.js
@@ -5,6 +5,18 @@ const groupSchema = Joi.object({
   ativo: Joi.bool().required(),
 });
 
+const customerSchema = Joi.object({
+  nomeCliente: Joi.string().required(),
+  tipo: Joi.string().required(),
+  ativo: Joi.bool().required(),
+  grupo: Joi.string().required(),
+  documentosCliente: Joi.array().items(Joi.object({
+    nomeDocumento: Joi.string().required(),
+    dadoDocumento: Joi.string().required(),
+  })).required(),
+});
+
 module.exports = {
   groupSchema,
+  customerSchema,
 };
