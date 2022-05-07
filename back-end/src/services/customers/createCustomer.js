024420676f2ea8652c28fe189328ca34ca892c7b
Demontier <demontier@outlook.com>
2022-05-07 11:26:21 -0300

    refactor(services): createCustomer

--- back-end/src/services/customers/createCustomer.js
+++ back-end/src/services/customers/createCustomer.js
@@ -1,35 +1,55 @@
 const { clientes, documentos, documentosClientes, grupos } = require('../../database/models');
 const { customerSchema } = require('../../utils/schemas');
-const { badRequest } = require('../../utils/statusCode');
+const { Conflict, badRequest } = require('../../utils/statusCode');
 const errorConstructor = require('../../utils/errorHandling');
 
 // eslint-disable-next-line max-lines-per-function
-module.exports = async (data) => {
-  const { nomeCliente, tipo, ativo, grupo, documentosCliente } = data;
+const handleDocument = (document, client) => {
+  document.forEach(async ({ nomeDocumento, dadoDocumento }) => {
+    let getDocument = await documentos.findOne({ where: { nomeDocumento } });
+    console.log(getDocument);
+    if (!getDocument) {
+      getDocument = await documentos.create({
+        nomeDocumento,
+      });
+    }
 
+    await documentosClientes.create({
+      clienteId: client.id,
+      documentoId: getDocument.id,
+      dadoDocumento,
+    });
+  });
+};
+
+const handleError = async (data) => {
+  const { nomeCliente, tipo, ativo, grupo, documentosCliente } = data;
+  
   const { error } = customerSchema.validate({ nomeCliente, tipo, ativo, grupo, documentosCliente });
   if (error) throw errorConstructor(badRequest, error.message);
 
-  const getGroup = await grupos.findOne({ where: { nome: grupo } });
+  // faz um array contendo os dados dos documentos cadastrados
+  const arrayDocuments = documentosCliente.map(({ dadoDocumento }) => dadoDocumento);
+  const documentExists = await documentosClientes
+  .findAll({ where: { dadoDocumento: arrayDocuments } });
 
-  const cliente = await clientes.create({
+  // lança um erro caso o cliente já tenha um documento cadastrado
+  if (documentExists.length > 0) throw errorConstructor(Conflict, 'Documento já cadastrado');
+};
+
+module.exports = async (data) => {
+  const { nomeCliente, tipo, ativo, grupo, documentosCliente } = data;
+  await handleError(data);
+
+  const getGroup = await grupos.findOne({ where: { nome: grupo } });
+  const client = await clientes.create({
     nomeCliente,
     ativo,
     tipo,
     grupoId: getGroup.id,
   });
 
-  documentosCliente.forEach(async ({ nomeDocumento, dadoDocumento }) => {
-    const documento = await documentos.create({
-      nomeDocumento,
-    });
-
-    await documentosClientes.create({
-      clienteId: cliente.id,
-      documentoId: documento.id,
-      dadoDocumento,
-    });
-  });
+  handleDocument(documentosCliente, client);
 
   return null;
 };
