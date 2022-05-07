c57fc40a6f86b98d4d8db675d67453695ce928d0
Demontier <demontier@outlook.com>
2022-05-07 16:29:38 -0300

    feat: adiciona a rota para deletar um cliente

--- back-end/src/router/customers/index.js
+++ back-end/src/router/customers/index.js
@@ -3,6 +3,7 @@ const {
   createCustomer,
   getAllCustomer,
   editCustomer,
+  deleteCustomer,
 } = require('../../controllers/customers');
 
 const customersRoutes = express.Router();
@@ -10,5 +11,6 @@ const customersRoutes = express.Router();
 customersRoutes.post('/', createCustomer);
 customersRoutes.get('/', getAllCustomer);
 customersRoutes.put('/:id', editCustomer);
+customersRoutes.delete('/:id', deleteCustomer);
 
 module.exports = customersRoutes;
