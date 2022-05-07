b6f98af2a5c891bfca4e8efbdcac771816705735
Demontier <demontier@outlook.com>
2022-05-07 13:21:32 -0300

    feat: adiciona a rota para editar cliente

--- back-end/src/router/customers/index.js
+++ back-end/src/router/customers/index.js
@@ -2,11 +2,13 @@ const express = require('express');
 const {
   createCustomer,
   getAllCustomer,
+  editCustomer,
 } = require('../../controllers/customers');
 
 const customersRoutes = express.Router();
 
 customersRoutes.post('/', createCustomer);
 customersRoutes.get('/', getAllCustomer);
+customersRoutes.put('/:id', editCustomer);
 
 module.exports = customersRoutes;
