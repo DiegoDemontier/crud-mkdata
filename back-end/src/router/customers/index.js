6f8af29bd1e3dda3049fd2dc74b3cd49a87981c7
Demontier <demontier@outlook.com>
2022-05-07 11:28:23 -0300

    feat: adiciona a rota para pegar todos os clientes

--- back-end/src/router/customers/index.js
+++ back-end/src/router/customers/index.js
@@ -1,10 +1,12 @@
 const express = require('express');
 const {
   createCustomer,
+  getAllCustomer,
 } = require('../../controllers/customers');
 
 const customersRoutes = express.Router();
 
 customersRoutes.post('/', createCustomer);
+customersRoutes.get('/', getAllCustomer);
 
 module.exports = customersRoutes;
